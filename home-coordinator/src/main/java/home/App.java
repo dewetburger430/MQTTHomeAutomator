013ccd01660303df84e62e2af8ca8f019286f872
Dewet Burger <57727889+dewetburger430@users.noreply.github.com>
2020-01-06 19:29:16 +0200

    Cleanup app

--- home-coordinator/src/main/java/home/App.java
+++ home-coordinator/src/main/java/home/App.java
@@ -1,23 +1,16 @@
 package home;
 
-import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.InputStreamReader;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ServerValue;
-import com.google.firebase.database.ValueEventListener;
 
-import home.device.IOPort.Power;
-import home.device.Device;
 import home.device.DeviceManager;
 
 /**
@@ -53,59 +46,19 @@ public final class App {
 
         FirebaseApp.initializeApp(firebaseOptions);
 
-        // The app only has access as defined in the Security Rules
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("/some_resource");
-        ref.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                String res = dataSnapshot.getValue().toString();
-                System.out.println(res);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // TODO Auto-generated method stub
-
-            }
-        });
-
+        // Overall contoller
         DatabaseReference controllerRef = FirebaseDatabase.getInstance().getReference("/controller");
         controllerRef.child("connected").setValueAsync("ONLINE");
         controllerRef.child("connected").onDisconnect().setValueAsync("OFFLINE");
         controllerRef.child("connectedChanged").setValueAsync(ServerValue.TIMESTAMP);
         controllerRef.child("connectedChanged").onDisconnect().setValueAsync(ServerValue.TIMESTAMP);
 
+        // Device controller
         DatabaseReference deviceRef = FirebaseDatabase.getInstance().getReference("/devices");
-
-        // MQTT
-        String mqttAddress = System.getenv("mqttAddress");
-        String mqttPort = System.getenv("mqttPort");
-        LOG.info("MQTT Server: " + mqttAddress + ":" + mqttPort);
-
         DeviceManager deviceManager = new DeviceManager(deviceRef);
 
-        final Device s = deviceManager.getDeviceByTopic("front-door-light-switch");
-
-        System.out.println("Press 1,2,3 to toggle switch, anything else to exit...");
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        while (true) {
-            String command = br.readLine();
-            switch (command) {
-            case "1":
-                s.getPort("Power1").setPower(Power.TOGGLE);
-                break;
-            case "2":
-                s.getPort("Power2").setPower(Power.TOGGLE);
-                break;
-            case "3":
-                s.getPort("Power3").setPower(Power.TOGGLE);
-                break;
-            default:
-                LOG.info("Closing connections");
-                br.close();
-                deviceManager.close();
-                return;
-            }
-        }
+        System.out.println("Press enter to exit...");
+        System.in.read();
+        deviceManager.close();
     }
 }
